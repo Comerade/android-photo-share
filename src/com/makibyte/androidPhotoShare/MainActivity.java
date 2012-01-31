0d922cf4936774d9f578a4a4ddf9e642417d6e38
Paul Maki <paul.maki@gmail.com>
2012-01-31 10:25:29 -0800

    Gridview loads sample images

--- src/com/makibyte/androidPhotoShare/MainActivity.java
+++ src/com/makibyte/androidPhotoShare/MainActivity.java
@@ -2,12 +2,24 @@ package com.makibyte.androidPhotoShare;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.GridView;
 
 public class MainActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
+  /** Called when the activity is first created. */
+  @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.main);
+      
+      GridView gridview = (GridView) findViewById(R.id.gridview);
+      gridview.setAdapter(new ImageAdapter(this));
+      
+      /* 
+      gridview.setOnItemClickListener(new OnItemClickListener() {
+    	  public void onItemCLick(AdapterView<?> parent, View v, int position, long id) {
+    		  Toast.makeText(MainActivity.this, "" + position, Toast.LENGTH_SHORT).show();
+    	  }
+      });
+      */
     }
 }
